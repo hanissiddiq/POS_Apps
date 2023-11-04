3be82a0e346154f4f3b309ce5308d325d38b488b
hanis siddiq <hanissiddiq10@gmail.com>
2023-11-04 13:37:03 +0700

    modified login page

---
+++ lib/pages/welcome.dart
@@ -0,0 +1,148 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:animated_login_v1/pages/login.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            FadeInDown(
+              delay: const Duration(milliseconds: 800),
+              duration: const Duration(milliseconds: 800),
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 2.w, vertical: 1.h),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    'assets/images/loginImage.png',
+                    width: 100.w,
+                    height: 50.h,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(color: Colors.white),
+              padding: EdgeInsets.symmetric(horizontal: 7.w, vertical: 2.h),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 1.6.w),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          FadeInUp(
+                            delay: const Duration(milliseconds: 700),
+                            duration: const Duration(milliseconds: 800),
+                            child: Text(
+                              'Point Of Sales App v1.0',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 20.sp, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 1.h,
+                          ),
+                          FadeInUp(
+                            delay: const Duration(milliseconds: 900),
+                            duration: const Duration(milliseconds: 1000),
+                            child: Text(
+                              'Lorem ipsum dolor sit amet ameer consectetur adipiscing elit',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 15.sp, fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                        ]),
+                  ),
+                  SizedBox(
+                    height: 4.h,
+                  ),
+                  FadeInUp(
+                    delay: const Duration(milliseconds: 1000),
+                    duration: const Duration(milliseconds: 1100),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const LoginPage(),
+                                  ));
+                            },
+                            style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                textStyle: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    fontFamily: 'Satoshi'),
+                                backgroundColor: const Color(0xFF835DF1),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 16)),
+                            child: FadeInUp(
+                                delay: const Duration(milliseconds: 1100),
+                                duration: const Duration(milliseconds: 1200),
+                                child: const Text('Masuk')),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  FadeInUp(
+                    delay: const Duration(milliseconds: 1100),
+                    duration: const Duration(milliseconds: 1200),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Anda belum punya akun ?',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Daftar',
+                              style: TextStyle(
+                                color: Color(0xFF835DF1),
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
